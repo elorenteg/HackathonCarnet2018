9d4530d0ed94a8b75051fc294a723d6cbea8ed63
Marc Vila <marcvilagomez@gmail.com>
2018-03-10 22:30:33 +0100

    Bluetooth working

--- android/app/src/main/java/com/bvisible/carnet/MainActivity.java
+++ android/app/src/main/java/com/bvisible/carnet/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.bvisible.carnet.controllers.BluetoothController;
 import com.bvisible.carnet.controllers.TPGraphDatabase;
 import com.bvisible.carnet.controllers.TPGraphQueryNearTP;
 import com.bvisible.carnet.models.Route;
@@ -24,14 +25,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AsyncResponse {
+public class MainActivity extends AppCompatActivity implements AsyncResponse, BluetoothController.ReadReceived, BluetoothController.BluetoothStatus {
 
     private static final String GRAPH_DATABASE_NAME = "imdb.gdb";
     public static String TAG = "MainActivity";
     private final String SparkseeLicense = "NWNRP-J7NZ0-7159N-FJG09";
+    private BluetoothController bluetoothController;
     TPGraphQueryNearTP asyncTask;
 
     private TextView mTextMessage;
+    private BluetoothController.ReadReceived readReceivedCallback = this;
+    private BluetoothController.BluetoothStatus bluetoothStatusCallback = this;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -87,7 +91,8 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
                 boolean allPermissionsChecked = report.getGrantedPermissionResponses().size() == permissions;
 
                 if (allPermissionsChecked) {
-                    openGDB();
+                    startBluetooth();
+                    //openGDB();
                 }
             }
 
@@ -104,8 +109,6 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         try {
             tpGraphDB.loadDatabase();
             asyncTask.queryGraph(tpGraphDB, 41.388693, 2.112126);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -125,5 +128,31 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             }
         }
         mTextMessage.setText(text);
+    }
+
+    private void startBluetooth() {
+        bluetoothController = BluetoothController.getInstance(this);
+        bluetoothController.startServices();
+        bluetoothController.setCallbacks(bluetoothStatusCallback, readReceivedCallback);
+    }
+
+    private void stopBluetooth() {
+        bluetoothController.stopAll();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopBluetooth();
+    }
+
+    @Override
+    public void onBluetoothChanged(int bluetoothStatus) {
+        Log.e(TAG, "BluetoothStatus: " + bluetoothStatus);
+    }
+
+    @Override
+    public void onReadReceived(String valueReceived) {
+        Log.e(TAG, "ReadReceived: " + valueReceived);
     }
 }
