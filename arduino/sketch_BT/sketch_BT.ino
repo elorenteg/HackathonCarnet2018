59572d6018b21437ba2a669804dd69ad5ea58004
Marc Vila <marcvilagomez@gmail.com>
2018-03-11 04:39:14 +0100

    Adding shock to Arduino

--- arduino/sketch_BT/sketch_BT.ino
+++ arduino/sketch_BT/sketch_BT.ino
@@ -9,18 +9,31 @@
  *  
  *  Button - Arduino
  *  When pressed, communicates the 5V to the 2 pin of the Arduino.
- *  5V - 2
- *  X  - 10kOhm + GND
+ *  (5V) - (2)
+ *  ()   - (10kOhm + GND)
+ *  
+ *  Shock - Arduino
+ *  When moved, change the D0 from HIGH to LOW (Or inverse)
+ *  5V  - Vcc
+ *  GND - GND
+ *  D0 - D6  
  */
 #include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
 SoftwareSerial BT(10,11);    // Definimos los pines RX y TX del Arduino conectados al Bluetooth
 
+const int shockAlarmTime = 250; 
+unsigned long lastShockTime;
+boolean bAlarm = false;
+
 const int buttonPin = 2;
 int buttonState = 0;
 
+const int shockPin = 6;
+int shockState = 0;
+
 void setup(){
-  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  pinMode(shockPin, INPUT);
   
   BT.begin(9600);       // Inicializamos el puerto serie BT que hemos creado
   Serial.begin(9600);   // Inicializamos el puerto serie
@@ -28,7 +41,8 @@ void setup(){
   Serial.println("Start BT, Name is HC-06, MAC is 20:15:10:20:04:46");
 }
 
-void manageBT() {
+void loop(){
+  // Gestion del BT
   // Si llega un dato por el puerto BT se envía al monitor serial
   if(BT.available()){
     Serial.write(BT.read());
@@ -38,16 +52,24 @@ void manageBT() {
   if(Serial.available()){
      BT.write(Serial.read());
   }
-}
- 
-void loop(){
-  manageBT();
 
-  // read the state of the pushbutton value:
+  // Gestion del boton
   buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     BT.write("Apretado\r");
+  }
+
+  // Gestion del sensor de shock
+  shockState = digitalRead (shockPin);
+  if (shockState == LOW) {
+    lastShockTime = millis();
+    if (!bAlarm) {
+      BT.write("Agitado\r");
+      bAlarm = true;
+    }
+  } else {
+    if((millis()-lastShockTime) > shockAlarmTime && bAlarm) {
+      bAlarm = false;
+    }
   }
 }
